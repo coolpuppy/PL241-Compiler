ebfae5c68c9bdb782eb24b308626e3418e9d8b3b
littledog623 <hyjiangthu@gmail.com>
2015-03-12 18:03:50 -0700

    test case 10 11

--- src/pl241/uci/edu/frontend/Parser.java
+++ src/pl241/uci/edu/frontend/Parser.java
@@ -851,7 +851,7 @@ public class Parser {
     }
 
     public static void main(String []args) throws Throwable{
-        String testname = "test011";
+        String testname = "test010";
         Parser p = new Parser("src/test/"+testname +".txt");
         p.parser();
         ControlFlowGraph.printInstruction();
